98dcec7cd5e81a3504ec780906c847dafdcdabc3
WuYifanX <510182912@qq.com>
2020-07-30 23:09:55 +0800

    finish 385

--- 385.迷你语法分析器.go
+++ 385.迷你语法分析器.go
@@ -1,8 +1,21 @@
+package main
+
+import (
+	"strconv"
+)
+
 /*
  * @lc app=leetcode.cn id=385 lang=golang
  *
  * [385] 迷你语法分析器
  */
+type NestedInteger struct {
+}
+
+func (n NestedInteger) IsInteger() bool {}
+
+func (n *NestedInteger) SetInteger(value int)   {}
+func (n *NestedInteger) Add(elem NestedInteger) {}
 
 // @lc code=start
 /**
@@ -31,7 +44,34 @@
  * func (n NestedInteger) GetList() []*NestedInteger {}
  */
 func deserialize(s string) *NestedInteger {
+	result := &NestedInteger{}
+	if len(s) == 0 {
+		return result
+	}
+	if s[0] != '[' {
+		value, _ := strconv.Atoi(s)
+		result.SetInteger(value)
+		return result
+	}
 
+	if len(s) <= 2 {
+		return result
+	}
+
+	start := 1
+	depth := 0
+
+	for i := 1; i < len(s); i++ {
+		if depth == 0 && (s[i] == ',' || i == len(s)-1) {
+			result.Add(*deserialize(s[start:i]))
+			start = i + 1
+		} else if s[i] == '[' {
+			depth++
+		} else if s[i] == ']' {
+			depth--
+		}
+	}
+	return result
 }
-// @lc code=end
 
+// @lc code=end
