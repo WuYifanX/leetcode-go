fc581ad23938ab60ba387033081d03d7c5d75909
WuYifanX <510182912@qq.com>
2020-08-31 23:14:54 +0800

    finish 380

---
+++ 380.常数时间插入、删除和获取随机元素.go
@@ -0,0 +1,119 @@
+import (
+	"math/rand"
+)
+
+/*
+ * @lc app=leetcode.cn id=380 lang=golang
+ *
+ * [380] 常数时间插入、删除和获取随机元素
+ *
+ * https://leetcode-cn.com/problems/insert-delete-getrandom-o1/description/
+ *
+ * algorithms
+ * Medium (48.51%)
+ * Likes:    156
+ * Dislikes: 0
+ * Total Accepted:    14.5K
+ * Total Submissions: 29.9K
+ * Testcase Example:  '["RandomizedSet","insert","remove","insert","getRandom","remove","insert","getRandom"]\n' +
+  '[[],[1],[2],[2],[],[1],[2],[]]'
+ *
+ * 设计一个支持在平均 时间复杂度 O(1) 下，执行以下操作的数据结构。
+ *
+ *
+ * insert(val)：当元素 val 不存在时，向集合中插入该项。
+ * remove(val)：元素 val 存在时，从集合中移除该项。
+ * getRandom：随机返回现有集合中的一项。每个元素应该有相同的概率被返回。
+ *
+ *
+ * 示例 :
+ *
+ *
+ * // 初始化一个空的集合。
+ * RandomizedSet randomSet = new RandomizedSet();
+ *
+ * // 向集合中插入 1 。返回 true 表示 1 被成功地插入。
+ * randomSet.insert(1);
+ *
+ * // 返回 false ，表示集合中不存在 2 。
+ * randomSet.remove(2);
+ *
+ * // 向集合中插入 2 。返回 true 。集合现在包含 [1,2] 。
+ * randomSet.insert(2);
+ *
+ * // getRandom 应随机返回 1 或 2 。
+ * randomSet.getRandom();
+ *
+ * // 从集合中移除 1 ，返回 true 。集合现在包含 [2] 。
+ * randomSet.remove(1);
+ *
+ * // 2 已在集合中，所以返回 false 。
+ * randomSet.insert(2);
+ *
+ * // 由于 2 是集合中唯一的数字，getRandom 总是返回 2 。
+ * randomSet.getRandom();
+ *
+ *
+*/
+
+// @lc code=start
+type RandomizedSet struct {
+	array []int
+	maps  map[int]int
+}
+
+/** Initialize your data structure here. */
+func Constructor() RandomizedSet {
+	return RandomizedSet{
+		array: make([]int, 0),
+		maps:  make(map[int]int),
+	}
+}
+
+/** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+func (this *RandomizedSet) Insert(val int) bool {
+
+	if !this.contains(val) {
+		this.array = append(this.array, val)
+		this.maps[val] = len(this.array) - 1
+		return true
+	} else {
+		return false
+	}
+}
+
+/** Removes a value from the set. Returns true if the set contained the specified element. */
+func (this *RandomizedSet) Remove(val int) bool {
+
+	if this.contains(val) {
+		index := this.maps[val]
+
+		this.array[index] = this.array[len(this.array)-1]
+		this.maps[this.array[index]] = index
+
+		this.array = this.array[:len(this.array)-1]
+		delete(this.maps, val)
+		return true
+	} else {
+		return false
+	}
+}
+
+func (this *RandomizedSet) contains(val int) bool {
+	_, isExist := this.maps[val]
+	return isExist
+}
+
+/** Get a random element from the set. */
+func (this *RandomizedSet) GetRandom() int {
+	return this.array[rand.Intn(len(this.array))]
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Insert(val);
+ * param_2 := obj.Remove(val);
+ * param_3 := obj.GetRandom();
+ */
+// @lc code=end
